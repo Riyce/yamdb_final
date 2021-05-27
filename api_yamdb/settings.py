c756d4282e2c230b7e2e02c07e4e078a2d76ebe2
kuzminov.f <riyce@mail.ru>
2021-05-27 12:41:15 +0300

    second try

--- api_yamdb/settings.py
+++ api_yamdb/settings.py
@@ -24,12 +24,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('SECRET_KEY')
+SECRET_KEY = 'p&l%385148kslhtyn^##a1)ilz@4zqj=rq&agdol^##zgl9(vs'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS')
+ALLOWED_HOSTS = ['127.0.0.1', '178.154.214.1', 'web']
 
 # Application definition
 
