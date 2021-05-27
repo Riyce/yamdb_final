96f816014e476bd2c5ca2837d1d8455bd6fe3110
kuzminov.f <riyce@mail.ru>
2021-05-27 15:07:55 +0300

    fix

--- api_yamdb/settings.py
+++ api_yamdb/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'p&l%385148kslhtyn^##a1)ilz@4zqj=rq&agdol^##zgl9(vs'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1', '178.154.214.1', 'web', 'selkieproject.tk']
+ALLOWED_HOSTS = ['web', 'localhost', '127.0.0.1', '178.154.214.1', 'selkieproject.tk']
 
 # Application definition
 
